b83229ee60d07bc076ae3ae9e18b372a32102365
JustinOcassionallycodes <jdreyer269@gmail.com>
2022-09-14 09:17:21 +0200

    React_project

---
+++ Portfolio_Gallery/src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./portfolio-gallery/Gallery";
+
+class App extends React.Component {
+  render(){
+  return (
+    <Gallery />
+  );
+  }
+}
+export default App;
+
---
+++ Portfolio_Gallery/src/portfolio-gallery/Data.js
@@ -0,0 +1,123 @@
+const data =[
+    {
+        id:1,
+        image:"./images/html.png",
+        title:"HTML Tutorial",
+        description:"HTML for Frontend. ",
+        category:"frontend",
+       
+    },
+    {
+        id:2,
+        image:"./images/css.png",
+        title:"CSS Tutorial",
+        description:"CSS for Frontend. ",
+        category:"frontend",
+       
+    },
+    {
+        id:3,
+        image:"./images/javascript.png",
+        title:"JS Tutorial",
+        description:"JavaScript for Frontend ",
+        category:"frontend",
+       
+    },
+    {
+        id:4,
+        image:"./images/react.jpg",
+        title:"React Js Tutorial",
+        description:"React for Frontend. ",
+        category:"frontend",
+       
+    },
+    {
+        id:5,
+        image:"./images/bootstrap.jpg",
+        title:"Bootstrap Tutorial",
+        description:"Bootstrap for Frontend. ",
+        category:"frontend",
+       
+    },
+    {
+        id:6,
+        image:"./images/php.png",
+        title:"PHP Tutorial",
+        description:"PHP for Backend. ",
+        category:"backend",
+       
+    },
+    {
+        id:7,
+        image:"./images/node-js.png",
+        title:"Node.js Tutorial",
+        description:"Node.js for Backend ",
+        category:"backend",
+       
+    },
+    {
+        id:8,
+        image:"./images/asp.jpg",
+        title:"ASP Tutorial",
+        description:"ASP for Backend. ",
+        category:"backend",
+       
+    },
+    {
+        id:9,
+        image:"./images/sql.jpg",
+        title:"SQL Tutorial",
+        description:"SQL for Database ",
+        category:"database",
+       
+    },
+    {
+        id:10,
+        image:"./images/mongodb.png",
+        title:"Mongo DB Tutorial",
+        description:"Mongo DB for Database ",
+        category:"database",
+       
+    },
+    {
+        id:11,
+        image:"./images/c.png",
+        title:"C Tutorial",
+        description:"C for Programming ",
+        category:"programming",
+       
+    },
+    {
+        id:12,
+        image:"./images/c-plus.png",
+        title:"C++ Tutorial",
+        description:"C++ for Programming ",
+        category:"programming",
+       
+    },
+    {
+        id:13,
+        image:"./images/java.png",
+        title:"Java Tutorial",
+        description:"Java for Programming ",
+        category:"programming",
+       
+    },
+    {
+        id:14,
+        image:"./images/python.png",
+        title:"Python Tutorial",
+        description:"Python for Programming ",
+        category:"programming",
+       
+    },
+    {
+        id:15,
+        image:"./images/go.png",
+        title:"Go Tutorial",
+        description:"Go for Programming",
+        category:"programming",
+       
+    },
+]
+export default data;
