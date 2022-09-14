9582a0fe97147cc1cd2a80a64fd5dbfc4b434a79
JustinOcassionallycodes <jdreyer269@gmail.com>
2022-09-14 09:07:49 +0200

    React_project

---
+++ Dynamic_Table/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from "./dynamic-table/DynamicTable";
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ Dynamic_Table/src/dynamic-table/TableData.js
@@ -0,0 +1,8 @@
+const TableData=[
+    {id:1, fullName:"Moe Blessing", age:25, city:"Johannesburg"},
+    {id:2, fullName:"Clayton Ninow", age:26, city:"Durban"},
+    {id:3, fullName:"Jada", age:18, city:"Hawaii"},
+    {id:4, fullName:"Olivia", age:22, city: "Toronto"},
+    {id:5, fullName:"Bianca Denver", age: 21, city: "Smores"}
+]
+export default TableData;
